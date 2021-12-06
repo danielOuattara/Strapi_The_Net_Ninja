044a61a7b734f65e8f1ba864dc1237006d470dfc
daniel ouattara <daniel.ouattara@gmx.com>
2021-12-06 08:53:40 +0100

    strapi SET 1-4 ; starting react front from 5

---
+++ 05_Adding_React_Front_App/frontend/src/App.js
@@ -0,0 +1,16 @@
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Home from "./pages/Home";
+import ReviewDetails from "./pages/ReviewDetails";
+import Category from "./pages/Category";
+import SiteHeader from "./components/SiteHeader";
+
+
+function App() {
+  return (
+    <div className="App">
+
+    </div>
+  );
+}
+
+export default App;
---
+++ 05_Adding_React_Front_App/frontend/src/components/SiteHeader.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+
+function SiteHeader() {
+    return (
+        <div className="site-header">
+            <Link to="/">Ninja  Review</Link>           
+        </div>
+    );
+}
+
+export default SiteHeader
