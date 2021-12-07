0af2256ea2a3e19288920fda84dbcf0dbefc96e6
daniel ouattara <daniel.ouattara@gmx.com>
2021-12-07 17:37:21 +0100

    5 strapi: fullstaxk layout ready, front router ok,

--- 05_Adding_React_Front_App/frontend/src/App.js
+++ 05_Adding_React_Front_App/frontend/src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import ReviewDetails from "./pages/ReviewDetails";
 import Category from "./pages/Category";
@@ -7,9 +7,16 @@ import SiteHeader from "./components/SiteHeader";
 
 function App() {
   return (
-    <div className="App">
-
-    </div>
+    <>
+      <BrowserRouter>
+        <SiteHeader />
+        <Routes>
+          <Route path="/" exact element={<Home />} />
+          <Route path="/details/:id" element={<ReviewDetails />} />
+          <Route path="/category/:id" element={<Category />} />
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
--- 05_Adding_React_Front_App/frontend/src/components/SiteHeader.js
+++ 05_Adding_React_Front_App/frontend/src/components/SiteHeader.js
@@ -1,13 +1,12 @@
-import React from 'react';
 import { Link } from 'react-router-dom';
 
 
 function SiteHeader() {
     return (
         <div className="site-header">
-            <Link to="/">Ninja  Review</Link>           
+            <Link to="/"> <h1>Ninja Review</h1></Link>           
         </div>
     );
 }
 
-export default SiteHeader
+export default SiteHeader;
