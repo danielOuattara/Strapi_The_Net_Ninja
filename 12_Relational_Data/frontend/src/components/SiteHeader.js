1a5b734b6353d39b3deb7fc1c6f359c8850b1259
daniel ouattara <daniel.ouattara@gmx.com>
2021-12-10 00:47:47 +0100

    built relation: many-to-many review<=>category

---
+++ 12_Relational_Data/frontend/src/components/SiteHeader.js
@@ -0,0 +1,39 @@
+import { Link } from 'react-router-dom';
+import {useQuery, gql } from "@apollo/client";
+
+const CATEGORIES = gql`
+    query GetCategories {
+        categories {
+            data {
+                id
+                attributes {
+                    name
+                }
+            }
+        }
+    }
+`;
+
+function SiteHeader() {
+
+    const { loading, error, data } = useQuery(CATEGORIES);
+    // if(loading) return <p>Loading ...</p>;
+    if(error) return <p>{error} ...</p>;
+
+    return (
+        <div className="site-header">
+            <Link to="/"> <h1>Ninja Review</h1></Link>
+           {!loading && !error && <nav className="categories">
+                <span>Filter reviews by category : </span>
+                {data.categories.data.map( (category) =>{
+                    return <Link key={category.id}  to={`/category/${category.id}`}> 
+                        {category.attributes.name} &nbsp;| &nbsp; 
+                    </Link>;
+                } )}
+            </nav> }
+                     
+        </div>
+    );
+}
+
+export default SiteHeader;
